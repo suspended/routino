ff5ab06f4a472278d46aee9a8c757b0a91859fa5
Brian <suspended@users.noreply.github.com>
2009-01-01 20:01:14 +0000

    Remove the functions to initialise the node and segment arrays.

--- src/planetsplitter.c
+++ src/planetsplitter.c
@@ -1,11 +1,11 @@
 /***************************************
- $Header: /home/amb/CVS/routino/src/planetsplitter.c,v 1.1 2008-12-31 12:20:55 amb Exp $
+ $Header: /home/amb/CVS/routino/src/planetsplitter.c,v 1.2 2009-01-01 20:01:14 amb Exp $
 
  OSM planet file splitter.
  ******************/ /******************
  Written by Andrew M. Bishop
 
- This file Copyright 2008 Andrew M. Bishop
+ This file Copyright 2008,2009 Andrew M. Bishop
  It may be distributed under the GNU Public License, version 2, or
  any higher version.  See section COPYING of the GNU Public license
  for conditions under which this file may be redistributed.
@@ -21,11 +21,6 @@
 
 int main(int argc,char** argv)
 {
- /* Create the variables */
-
- NewNodeList();
- NewSegmentList();
-
  /* Parse the file */
 
  ParseXML(stdin);
