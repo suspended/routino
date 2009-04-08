1b34538ec0cff9825e12071c98272bd1c23e6429
amb <amb@cdd41072-61d7-4102-955e-fb406ba21da9>
2009-04-08 16:54:34 +0000

    Changed the license to Affero GPLv3.

--- trunk/src/router.c
+++ trunk/src/router.c
@@ -1,14 +1,24 @@
 /***************************************
- $Header: /home/amb/CVS/routino/src/router.c,v 1.43 2009-03-23 18:47:18 amb Exp $
+ $Header: /home/amb/CVS/routino/src/router.c,v 1.44 2009-04-08 16:54:34 amb Exp $
 
  OSM router.
- ******************/ /******************
- Written by Andrew M. Bishop
 
+ Part of the Routino routing software.
+ ******************/ /******************
  This file Copyright 2008,2009 Andrew M. Bishop
- It may be distributed under the GNU Public License, version 2, or
- any higher version.  See section COPYING of the GNU Public license
- for conditions under which this file may be redistributed.
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ***************************************/
 
 
--- trunk/src/segments.h
+++ trunk/src/segments.h
@@ -1,14 +1,24 @@
 /***************************************
- $Header: /home/amb/CVS/routino/src/segments.h,v 1.29 2009-02-24 19:59:36 amb Exp $
+ $Header: /home/amb/CVS/routino/src/segments.h,v 1.30 2009-04-08 16:54:34 amb Exp $
 
  A header file for the segments.
- ******************/ /******************
- Written by Andrew M. Bishop
 
- This file Copyright 2009 Andrew M. Bishop
- It may be distributed under the GNU Public License, version 2, or
- any higher version.  See section COPYING of the GNU Public license
- for conditions under which this file may be redistributed.
+ Part of the Routino routing software.
+ ******************/ /******************
+ This file Copyright 2008,2009 Andrew M. Bishop
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ***************************************/
 
 
