459b0eddad2555bcfa6095df1a43f49237bed95b
Brian <suspended@users.noreply.github.com>
2013-01-21 18:53:32 +0000

    Remove unnecessary word from logerror messages.

--- src/tagging.c
+++ src/tagging.c
@@ -927,11 +927,11 @@ static void ApplyRules(TaggingRuleList *rules,TagList *input,TagList *output,con
                }
 
        if(current_list==&NodeRules)
-          logerror("Node %"PRIu64" has an unrecognised tag value '%s' = '%s' (in tagging rules); %s.\n",current_id,k,v,rules->rules[i].message);
+          logerror("Node %"PRIu64" has an unrecognised tag '%s' = '%s' (in tagging rules); %s.\n",current_id,k,v,rules->rules[i].message);
        if(current_list==&WayRules)
-          logerror("Way %"PRIu64" has an unrecognised tag value '%s' = '%s' (in tagging rules); %s.\n",current_id,k,v,rules->rules[i].message);
+          logerror("Way %"PRIu64" has an unrecognised tag '%s' = '%s' (in tagging rules); %s.\n",current_id,k,v,rules->rules[i].message);
        if(current_list==&RelationRules)
-          logerror("Relation %"PRIu64" has an unrecognised tag value '%s' = '%s' (in tagging rules); %s.\n",current_id,k,v,rules->rules[i].message);
+          logerror("Relation %"PRIu64" has an unrecognised tag '%s' = '%s' (in tagging rules); %s.\n",current_id,k,v,rules->rules[i].message);
       }
    }
 
