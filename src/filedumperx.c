b8742d3dc3707226bd8693cfe2f10f723c62b5aa
Brian <suspended@users.noreply.github.com>
2013-05-09 18:30:34 +0000

    Change datatype from signed to unsigned (pedantic compiler warning).

--- src/filedumperx.c
+++ src/filedumperx.c
@@ -3,7 +3,7 @@
 
  Part of the Routino routing software.
  ******************/ /******************
- This file Copyright 2008-2012 Andrew M. Bishop
+ This file Copyright 2008-2013 Andrew M. Bishop
 
  This program is free software: you can redistribute it and/or modify
  it under the terms of the GNU Affero General Public License as published by
@@ -220,7 +220,7 @@ static void print_ways(const char *filename)
     FILESORT_VARINT waysize;
     WayX wayx;
     char *name=NULL;
-    int malloced=0;
+    size_t malloced=0;
 
     ReadFile(fd,&waysize,FILESORT_VARSIZE);
 
