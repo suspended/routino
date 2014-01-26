dffe472fa941ca5b0b3c4a2cacf37228112312d1
amb <amb@cdd41072-61d7-4102-955e-fb406ba21da9>
2014-01-26 16:08:31 +0000

    Fix bug with Leaflet map startup.

--- trunk/web/www/routino/visualiser.leaflet.js
+++ trunk/web/www/routino/visualiser.leaflet.js
@@ -197,10 +197,7 @@ function map_init()             // called from visualiser.html
     map.setView(L.latLng(lat,lon),zoom);
    }
  else
-   {
-    map.setView(map.options.maxBounds.getCenter());
     map.fitBounds(map.options.maxBounds);
-   }
 
  // Unhide editing URL if variable set
 
