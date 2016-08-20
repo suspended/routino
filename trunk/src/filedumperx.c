e0bf6afa0c8a38ff714fdebc20f565a22ba53ef6
amb <amb@cdd41072-61d7-4102-955e-fb406ba21da9>
2016-08-20 16:24:36 +0000

    Bug fix for error detected by gcc-6.

--- trunk/src/filedumperx.c
+++ trunk/src/filedumperx.c
@@ -3,7 +3,7 @@
 
  Part of the Routino routing software.
  ******************/ /******************
- This file Copyright 2008-2015 Andrew M. Bishop
+ This file Copyright 2008-2016 Andrew M. Bishop
 
  This program is free software: you can redistribute it and/or modify
  it under the terms of the GNU Affero General Public License as published by
@@ -247,10 +247,10 @@ static void print_route_relations(const char *filename)
     while(!ReadFileBuffered(fd,&nodeid,sizeof(node_t)) && nodeid!=NO_NODE_ID)
        printf("  node=%"Pnode_t"\n",nodeid);
 
-    while(!ReadFileBuffered(fd,&wayid,sizeof(way_t)) && wayid!=NO_WAY_ID);
+    while(!ReadFileBuffered(fd,&wayid,sizeof(way_t)) && wayid!=NO_WAY_ID)
        printf("  way=%"Pway_t"\n",wayid);
 
-    while(!ReadFileBuffered(fd,&relationid,sizeof(relation_t)) && relationid!=NO_RELATION_ID);
+    while(!ReadFileBuffered(fd,&relationid,sizeof(relation_t)) && relationid!=NO_RELATION_ID)
        printf("  relation=%"Prelation_t"\n",relationid);
    }
 
